fc85682cb5f199a40a57215ba91c8ef8b121e024
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-15 22:18:23 +0200

    Created TodoItem.cs model file with content

---
+++ ToDoAvalonia.NETCoreAvalonia/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+namespace ToDoAvalonia.NETCoreAvalonia.Models
+{
+    public class TodoItem
+    {
+        public string Description { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
