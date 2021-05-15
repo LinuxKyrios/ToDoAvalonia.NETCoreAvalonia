0e98db512c1cad473e7eea5d0fed453a1c6a7322
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-15 23:40:29 +0200

    Created new view and swapped out the list model view

---
+++ ToDoAvalonia.NETCoreAvalonia/ViewModels/AddItemViewModel.cs
@@ -0,0 +1,7 @@
+namespace ToDoAvalonia.NETCoreAvalonia.ViewModels
+{
+    public class AddItemViewModel : ViewModelBase
+    {
+        public string Description { get; set; }
+    }
+}
--- ToDoAvalonia.NETCoreAvalonia/ViewModels/MainWindowViewModel.cs
+++ ToDoAvalonia.NETCoreAvalonia/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,30 @@
-﻿using ToDoAvalonia.NETCoreAvalonia.Services;
+﻿using ReactiveUI;
+using ToDoAvalonia.NETCoreAvalonia.Services;
+
+using ToDoAvalonia.NETCoreAvalonia.Services;
 
 namespace ToDoAvalonia.NETCoreAvalonia.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private ViewModelBase content;
+
         public MainWindowViewModel(Database db)
         {
-            List = new TodoListViewModel(db.GetItems());
+            Content = List = new TodoListViewModel(db.GetItems());
+        }
+
+        public ViewModelBase Content
+        {
+            get => content;
+            private set => this.RaiseAndSetIfChanged(ref content, value);
         }
         
         public TodoListViewModel List { get; }
+
+        public void AddItem()
+        {
+            Content = new AddItemViewModel();
+        }
     }
 }
