57a5304ce5a6c7582991502a53ca618db8cbefe7
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-15 21:33:34 +0200

    Initial commit

---
+++ ToDoAvalonia.NETCoreAvalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace ToDoAvalonia.NETCoreAvalonia.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
